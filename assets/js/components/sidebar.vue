bfee442d66ed4c9464469dcbdf767633a6e8aa03
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2025-02-20 17:36:27 +0100

    update vue and add axios

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,41 +1,73 @@
 <template>
-    <div :class="[$style.sidebar, 'p-3', 'mb-5']">
-        <h5 class="text-center">
-            Categories
-        </h5>
-        <ul class="nav flex-column mb4">
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="/"
-                >All Products</a>
-            </li>
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="#"
-                >Category A</a>
-            </li>
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="#"
-                >Category B</a>
-            </li>
-        </ul>
+    <div :class="[this.$style.component, 'p-3', 'mb-5']">
+        <div v-show="!collapsed">
+            <h5 class="text-center">
+                Categories
+            </h5>
+            <ul class="nav flex-column mb4">
+                <li class="nav-item">
+                    <a
+                        class="nav-link"
+                        href="/"
+                    >All Products</a>
+                </li>
+                <li
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    class="nav-item"
+                >
+                    <a
+                        :href="category.link"
+                        class="nav-link"
+                    >
+                        {{ category.name }}
+                    </a>
+                </li>
+            </ul>
+            <hr>
+        </div>
+        <div class="d-flex justify-content-end">
+            <button
+                class="btn btn-secondary btn-sm"
+                @click="$emit('toggle-collapsed')"
+                v-text="collapsed ? '>>' : '<< Collapse'"
+            />
+        </div>
     </div>
 </template>
-<script>
-export default {
+<script>export default {
     name: 'Sidebar',
+    props: {
+        collapsed: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            categories: [
+                {
+                    name: 'Dot Matrix Printers',
+                    link: '#',
+                },
+                {
+                    name: 'Iomega Zip Drives',
+                    link: '#',
+                },
+            ],
+        };
+    },
+
 };
 </script>
 
 <style lang="scss" module>
 @import '~styles/components/light-component';
 
-.sidebar {
+.component {
   @include light-component;
+
+
   ul {
     li a:hover {
       background: $blue-component-link-hover;
--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <aside class="col-xs-12 col-3">
-                <sidebar />
+            <aside :class="asideClass">
+                <sidebar
+                    :collapsed="sidebarCollapsed"
+                    @toggle-collapsed="toggleSidebarCollapsed"
+                />
             </aside>
-            <div class="col-xs-12 col-9">
+            <div :class="contentClass">
                 <catalog />
             </div>
         </div>
@@ -21,5 +24,24 @@ export default {
         Catalog,
         Sidebar,
     },
+    data() {
+        return {
+            sidebarCollapsed: false,
+        };
+    },
+    computed: {
+        asideClass() {
+            return this.sidebarCollapsed ? 'aside-collapsed' : 'col-xs-12 col-3';
+        },
+        contentClass() {
+            return this.sidebarCollapsed ? 'col-xs-12 col-11' : 'col-xs-12 col-9';
+        },
+    },
+    methods: {
+        toggleSidebarCollapsed() {
+            this.sidebarCollapsed = !this.sidebarCollapsed;
+        },
+    },
 };
+
 </script>
