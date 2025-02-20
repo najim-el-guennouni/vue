dd1e47cfb55e4dfa3cf9be4da4c81744c3c47422
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2025-02-20 18:08:10 +0100

    Product Listing Components

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -7,26 +7,28 @@
                 </h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col-xs-12 col-6 mb-2 pb-2">
-                TODO - load some products!
-            </div>
-        </div>
-        <div class="row">
-            <legend-component :title="legend" />
-        </div>
+        <product-list :products="products" />
     </div>
 </template>
 <script>
+import axios from 'axios';
 import LegendComponent from '@/components/legend';
+import ProductList from '@/components/product-list';
 
 export default {
+
     name: 'Catalog',
     components: {
         LegendComponent,
+        ProductList,
     },
     data: () => ({
+        products: [],
         legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
     }),
+    async  mounted() {
+        const response = await axios.get('/api/products');
+        this.products = response.data['hydra:member'];
+    },
 };
 </script>
